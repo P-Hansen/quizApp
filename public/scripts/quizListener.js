518096c15bb8efc799d9ef049412c54ec4fa34ce
P-hansen <philip.pt.hansen@gmail.com>
2021-04-05 02:22:33 +0000

    moved timer to js file

--- public/scripts/quizListener.js
+++ public/scripts/quizListener.js
@@ -1,18 +1,18 @@
 //listens during the quiz
 $(document).ready(function() {
 
-      //clicking the write a new tweet slides the compose section down
-  let downFlag = false;
-  $(".right").on("click", function(event) {
-    if (downFlag === false) {
-      $(".new-tweet").slideDown();
-      $("#tweet-text").focus();
-      downFlag = true;
-    } else {
-      $(".new-tweet").slideUp();
-      downFlag = false;
-    }
-  });
+    let s = 16;
+    const myTimer = () => {
+      s -= 1;
+      document.getElementById("question-timer").innerHTML = s;
+    };
+    let myVar = setInterval(myTimer, 1000);
 
-  
-}
+      //clicking the write a new tweet slides the compose section down
+    $("button").on("click", function(event) {
+        event.preventDefault();
+        $(this).parent().children().removeClass("clicked");
+        $(this).addClass("clicked");
+    });
+
+});
