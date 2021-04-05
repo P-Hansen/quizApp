6172917539b3225cf30a15a8db26ab0de7c4909a
Caden Choi <cadenc.7@gmail.com>
2021-04-05 04:40:12 +0000

    got console to push an object from trivia api

--- views/quizzes.js
+++ views/quizzes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const db = require('../db/dbConnection.js');
 const fetch = require('node-fetch');
 const pg = require('pg');
+const bodyParser = require('body-parser')
+
 
 //GET /quizzes
 router.get('/', (req, res) => {
@@ -12,12 +14,48 @@ router.get('/', (req, res) => {
 
 // const api_url = 'https://opentdb.com/api.php?amount=10&category=15&type=multiple'
 // async function getQuiz() {
-//   const response = await fetch(api_url);
+//   let response = await fetch(api_url);
 //   const data = await response.json();
 //   console.log(data);
-//   // console.log(JSON.parse(body))
+//   console.log(JSON.parse(body))
 // }
 // getQuiz();
+
+let questions = [];
+
+let api_url = 'https://opentdb.com/api.php?amount=10&category=15&type=multiple'
+fetch('https://opentdb.com/api.php?amount=10&category=15&type=multiple')
+.then((res) => {
+  return res.json();
+})
+.then((loadedQuestions) => {
+  questions = loadedQuestions.results.map((loadedQuestion) => {
+      const formattedQuestion = {
+          question: loadedQuestion.question,
+      };
+
+      const answerChoices = [...loadedQuestion.incorrect_answers];
+      formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+      answerChoices.splice(
+          formattedQuestion.answer - 1,
+          0,
+          loadedQuestion.correct_answer
+      );
+
+      answerChoices.forEach((choice, index) => {
+          formattedQuestion['Answer' + (index + 1)] = choice;
+      });
+      console.log(formattedQuestion)
+      return formattedQuestion;
+
+  });
+
+})
+.catch((err) => {
+  console.error(err);
+});
+
+
 
 // const connectionString = 'postgres://username:password@localhost/pg_demo_db' // your connection string
 
